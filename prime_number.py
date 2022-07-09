1adf4530ae756735a1a974c29b9938d0fd8607ee
PATILSHUBHAM69 <107848127+PATILSHUBHAM69@users.noreply.github.com>
2022-07-09 09:29:16 -0700

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -1,16 +1,18 @@
 list1 = [10,11,12,13,14,15]
-list_new= []
+list_not_prime= []
+list_prime= []
 
 def prime(num):
   if num > 1:
     for i in range(2,num):
       if num%i==0:
-        list_new.append(num)
+        list_not_prime.append(num)
         print(num,"is not prime number (",i,"times",num//i,"is",num,")")
         break
     else:
-        print(num, "is prime number")
+        print(list_prime.append(num), "is prime number")
 
 for item in list1:
   prime(item)
-print("not prime number",list_new)
+print("not prime number = ",list_not_prime)
+print("prime number is = ",list_prime)
